dddc55a63060ca31940e247c96c40a7076cd597c
Rafa37412 <rafacamara999@gmail.com>
2025-09-19 11:57:53 -0300

    4.1.1

--- src/main/java/com/projetocefoods/cefoods/model/Usuario.java
+++ src/main/java/com/projetocefoods/cefoods/model/Usuario.java
@@ -2,10 +2,8 @@ package com.projetocefoods.cefoods.model;
 
 import jakarta.persistence.*;
 import lombok.*;
-
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties({ "hibernateLazyInitializer", "handler" })
@@ -20,46 +18,58 @@ public class Usuario {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "idUsuario")
+    @Column(name = "id_usuario")
     private Long idUsuario;
 
+    @Column(name = "nome")
     private String nome;
+
+    @Column(name = "login")
     private String login;
+
+    @Column(name = "email")
     private String email;
+
+    @Column(name = "senha")
     private String senha;
+
+    @Column(name = "telefone")
     private String telefone;
+
+    @Column(name = "cpf")
     private String cpf;
 
-    @Column(name = "dataNascimento")
+    @Column(name = "data_nascimento")
     private LocalDate dataNascimento;
 
-    @Column(name = "tipoUsuario")
+    @Column(name = "tipo_usuario")
     private String tipoUsuario;
 
-    @Column(name = "tipoPerfil")
+    @Column(name = "tipo_perfil")
     private String tipoPerfil;
 
-    @Column(name = "chavePix")
+    @Column(name = "chave_pix")
     private String chavePix;
 
-    @Column(name = "fotoPerfil")
+    @Column(name = "foto_perfil")
     private String fotoPerfil;
 
-    @Column(name = "dataCadastro")
+    @Column(name = "data_cadastro")
     private LocalDateTime dataCadastro;
 
+    @Column(name = "ativo")
     private Boolean ativo;
 
-    @Column(name = "ultimoAcesso")
+    @Column(name = "ultimo_acesso")
     private LocalDateTime ultimoAcesso;
 
-    @Column(name = "emailVerificado")
+    @Column(name = "email_verificado")
     private Boolean emailVerificado;
 
-    @Column(name = "tokenRecuperacao")
+    @Column(name = "token_recuperacao")
     private String tokenRecuperacao;
 
-    @Column(name = "possuiLoja")
+    @Column(name = "possui_loja")
     private Boolean possuiLoja;
 
 }
